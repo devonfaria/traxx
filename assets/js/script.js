669bd772b843cac7037ff9ee51a86318c023b341
Devon Faria <devonfaria@gmail.com>
2022-03-09 11:51:47 -0500

    added fetch functions for API and handleSubmission function

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+var containerEL = document.querySelector('.results-container');
+
+// DEFINING FUNCTIONS
+var handleSubmission = function (event) {
+  event.preventDefault();
+  // removes HTML from weather bar
+  document.querySelector('.weather-bar').innerHTML = '';
+  var city = cityInputEl.value.trim();
+  // Translates city name to coordinates
+  getCoordinates(city);
+};
+
+// SEARCH FOR RESULTS
+var fetchTrackID = function (artist, song) {
+  // Element container to attach results
+  var resultContainerEl = document.querySelector('.result-container');
+
+  // Search track URL template
+  // var apiTrackID = `http://api.musixmatch.com/ws/1.1/track.search?apikey=fe0a8c874884f61f197aa259a3450876&q_artist=${artist}&q_track=${song}&page_size=20`;
+
+  var apiTrackID = `https://api.musixmatch.com/ws/1.1/track.search?apikey=fe0a8c874884f61f197aa259a3450876&q_artist=Ariana%20Grande&q_track=God%20is%20a%20woman&page_size=2`;
+
+  
+  // Fetching data from Musicmatch for search results; returning trackID
+  fetch (apiTrackID)
+    .then(function (response) {
+      if (response.ok) {
+      response.json()
+      .then(function (data) {
+        console.log(data.length);
+        for (var i = 0; i < data.length; i++) {
+          // Creating elements on loop
+          var resultEl = document.createElement('div');
+          var resultHeaderEl = document.createElement('h2');
+          var resultTextEl = document.createElement('p');
+          // Adding classes
+          resultEl.classList.add('result');
+          resultHeaderEl.classList.add('result-header');
+          resultTextEl.classList.add('.result-text')
+          // Adding text content - replace with data from API pull
+          resultHeaderEl.textContent = 'Toxic - Britney Spears'; 
+          resultTextEl.textContent = 'Lorem ipsisjw iwfwjjrwijjijvjrij ivjijis djivjijvisje ijwiejgfisih sdcfaehfhsfi sifhhfisdhfi  shfhifosh hosdhgo';
+          // Appending elements
+          resultEl.append(resultHeaderEl, resultTextEl);
+          resultContainerEl.append(resultEl)
+        };
+      });
+    }});
+};
+fetchTrackID(); 
+
+var fetchLyrics = function (trackID) {
+  // Element container to attach track information
+  var trackContainerEl = document.querySelector('.track-container');
+
+  // Search for lyrics URL template
+  var apiLyrics = `http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackID}&apikey=fe0a8c874884f61f197aa259a3450876`;
+  
+  // Fetching data from Musixmatch for search results
+  fetch (apiLyrics)
+    .then(function (response) {
+      if (response.ok) {
+      response.json()
+      .then(function (data) {
+        console.log(data.length);
+        for (var i = 0; i < data.length; i++) {
+          // Creating elements on loop
+          var trackEl = document.createElement('div');
+          var trackHeaderEl = document.createElement('h2');
+          var trackArtistEl = document.createElement('p');
+          var trackLyricsEl = document.createElement('p');
+          var videoEl = document.createElement('iframe')
+          // Adding classes
+          trackEl.classList.add('track');
+          trackHeaderEl.classList.add('track-header');
+          trackArtistEl.classList.add('tract-artist');
+          trackLyricsEl.classList.add('.track-text');
+          videoEl.classList.add('.video');
+          videoEl.src = 'https://www.youtube.com/watch?v=EDQVDCsrFAE';
+          // Adding text content - replace with data from API pull
+          trackHeaderEl.textContent = 'Toxic';
+          trackArtistEl.textContent = 'Britney Spears';
+          trackLyricsEl.textContent = 'Lorem ipsisjw iwfwjjrwijjijvjrij ivjijis djivjijvisje ijwiejgfisih sdcfaehfhsfi sifhhfisdhfi  shfhifosh hosdhgo';
+          // Appending elements
+          trackEl.append(trackHeaderEl, trackLyricsEl);
+          trackContainerEl.append(trackEl)
+        };
+      });
+    }});
+};
+fetchLyrics();
