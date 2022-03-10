7333435f3ced700e33c90bdb426c3dba77614ced
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-10 12:01:00 -0500

    added button search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,19 @@ var previousSearches = JSON.parse(localStorage.getItem('trackSearches')) || [];
 // DEFINING FUNCTIONS
 var handleSubmission = function (event) {
   event.preventDefault();
-  document.location()
+  location.replace('search-results.html')
   // removes HTML from results container
   var search = inputEl.value.trim();
   // Checks if search is original
-    if (previousSearches.includes(search)) {
-      search=search.replaceAll(" ","%20");
-    } else {
-      // If unique search, stores to localStorage
-      previousSearches.unshift(search);
-      var storage = JSON.stringify(previousSearches);
-      localStorage.setItem('trackSearches', storage);
-      search=search.replaceAll(" ","%20");
-    }
+  if (previousSearches.includes(search)) {
+    search = search.replaceAll(" ", "%20");
+  } else {
+    // If unique search, stores to localStorage
+    previousSearches.unshift(search);
+    var storage = JSON.stringify(previousSearches);
+    localStorage.setItem('trackSearches', storage);
+    search = search.replaceAll(" ", "%20");
+  }
   fetchTrackID(search);
 };
 
@@ -29,33 +29,34 @@ var fetchTrackID = function (searchTerm) {
   var apiTrackID = `https://devon-and-david-20220309.herokuapp.com/track.search?q_track=${searchTerm}&page_size=20`;
 
   // Fetching data from Musicmatch for search results; returning trackID
-  fetch (apiTrackID)
+  fetch(apiTrackID)
     .then(function (response) {
       if (response.ok) {
-      response.json()
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          // Creating elements on loop
-          console.log(data);
-          var lyrics = fetchLyrics(data[i].track.track_id);
-          console.log(lyrics);
-          var resultEl = document.createElement('div');
-          var resultHeaderEl = document.createElement('h2');
-          var resultTextEl = document.createElement('p');
-          // Adding classes
-          resultEl.classList.add('result');
-          resultHeaderEl.classList.add('result-header');
-          resultTextEl.classList.add('.result-text')
-          // Adding text content - replace with data from API pull
-          resultHeaderEl.textContent = `${data[i].track.track_name} - ${data[i].track.artist_name}`;
-          resultTextEl.textContent = `${lyrics}`;
-          // Appending elements
-          resultEl.append(resultHeaderEl, resultTextEl);
-          resultContainerEl.append(resultEl)
-        };
-        fetchLyrics(data[0].track.track_id)
-      });
-    }});
+        response.json()
+          .then(function (data) {
+            for (var i = 0; i < data.length; i++) {
+              // Creating elements on loop
+              console.log(data);
+              var lyrics = fetchLyrics(data[i].track.track_id);
+              console.log(lyrics);
+              var resultEl = document.createElement('div');
+              var resultHeaderEl = document.createElement('h2');
+              var resultTextEl = document.createElement('p');
+              // Adding classes
+              resultEl.classList.add('result');
+              resultHeaderEl.classList.add('result-header');
+              resultTextEl.classList.add('.result-text')
+              // Adding text content - replace with data from API pull
+              resultHeaderEl.textContent = `${data[i].track.track_name} - ${data[i].track.artist_name}`;
+              resultTextEl.textContent = `${lyrics}`;
+              // Appending elements
+              resultEl.append(resultHeaderEl, resultTextEl);
+              resultContainerEl.append(resultEl)
+            };
+            fetchLyrics(data[0].track.track_id)
+          });
+      }
+    });
 };
 
 var fetchLyrics = function (trackID) {
@@ -65,35 +66,36 @@ var fetchLyrics = function (trackID) {
 
   // Search for lyrics URL template
   var apiLyrics = `https://devon-and-david-20220309.herokuapp.com/track.lyrics.get?track_id=${trackID}`;
-  
+
   // Fetching data from Musixmatch for search results
-  fetch (apiLyrics)
+  fetch(apiLyrics)
     .then(function (response) {
       if (response.ok) {
-      response.json()
-      .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          // Creating elements on loop
-          var trackEl = document.createElement('div');
-          var trackHeaderEl = document.createElement('h2');
-          var trackArtistEl = document.createElement('p');
-          var trackLyricsEl = document.createElement('p');
-          // Adding classes
-          trackEl.classList.add('track');
-          trackHeaderEl.classList.add('track-header');
-          trackArtistEl.classList.add('tract-artist');
-          trackLyricsEl.classList.add('.track-text');
-          // Adding text content - replace with data from API pull
-          trackHeaderEl.textContent = 'Toxic';
-          trackArtistEl.textContent = 'Britney Spears';
-          trackLyricsEl.textContent = 'Lorem ipsisjw iwfwjjrwijjijvjrij ivjijis djivjijvisje ijwiejgfisih sdcfaehfhsfi sifhhfisdhfi  shfhifosh hosdhgo';
-          // Appending elements
-          trackEl.append(trackHeaderEl, trackArtistEl, trackLyricsEl);
-          trackContainerEl.append(trackEl);
-        };
-      });
-    }});
+        response.json()
+          .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+              // Creating elements on loop
+              var trackEl = document.createElement('div');
+              var trackHeaderEl = document.createElement('h2');
+              var trackArtistEl = document.createElement('p');
+              var trackLyricsEl = document.createElement('p');
+              // Adding classes
+              trackEl.classList.add('track');
+              trackHeaderEl.classList.add('track-header');
+              trackArtistEl.classList.add('tract-artist');
+              trackLyricsEl.classList.add('.track-text');
+              // Adding text content - replace with data from API pull
+              trackHeaderEl.textContent = 'Toxic';
+              trackArtistEl.textContent = 'Britney Spears';
+              trackLyricsEl.textContent = 'Lorem ipsisjw iwfwjjrwijjijvjrij ivjijis djivjijvisje ijwiejgfisih sdcfaehfhsfi sifhhfisdhfi  shfhifosh hosdhgo';
+              // Appending elements
+              trackEl.append(trackHeaderEl, trackArtistEl, trackLyricsEl);
+              trackContainerEl.append(trackEl);
+            };
+          });
+      }
+    });
 };
 
 var fetchVideo = function (variable) {
@@ -104,24 +106,25 @@ var fetchVideo = function (variable) {
 
   // Search for lyrics URL template
   var apiVideo = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerms}key=AIzaSyBjmcNPYyG9kBMKxEBBj5x6rjJ4yvMj18g`;
-  
+
   // Fetching data from Musixmatch for search results
-  fetch (apiVideo)
+  fetch(apiVideo)
     .then(function (response) {
       if (response.ok) {
-      response.json()
-      .then(function (data) {
-        console.log(data.length);
-        // Creating video element
-        var videoEl = document.createElement('iframe')
-        // Adding attributes
-        videoEl.classList.add('.video');
-        videoEl.src = 'https://www.youtube.com/watch?v=EDQVDCsrFAE';
-        // Appending elements
-        trackContainerEl.append(videoEl)
-        
-      });
-    }});
+        response.json()
+          .then(function (data) {
+            console.log(data.length);
+            // Creating video element
+            var videoEl = document.createElement('iframe')
+            // Adding attributes
+            videoEl.classList.add('.video');
+            videoEl.src = 'https://www.youtube.com/watch?v=EDQVDCsrFAE';
+            // Appending elements
+            trackContainerEl.append(videoEl)
+
+          });
+      }
+    });
 };
 
 formEl.addEventListener('submit', handleSubmission);
